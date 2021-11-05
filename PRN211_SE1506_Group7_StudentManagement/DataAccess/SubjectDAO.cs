fa7c2a56a19f051f21e497137a3a62fc16009f35
khanhlinh00 <65752339+khanhlinh00@users.noreply.github.com>
2021-11-05 14:15:34 +0700

    Add files via upload

--- PRN211_SE1506_Group7_StudentManagement/DataAccess/SubjectDAO.cs
+++ PRN211_SE1506_Group7_StudentManagement/DataAccess/SubjectDAO.cs
@@ -4,6 +4,9 @@ using Microsoft.Data.SqlClient;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 
 namespace DataAccess
