288acc6bb54643be6582e1cad15add4f8b6e1650
builinht <builinhaqua@gmail.com>
2021-11-05 10:35:17 +0700

    oh

--- PRN211_SE1506_Group7_StudentManagement/DataAccess/Repository/MarkRepository.cs
+++ PRN211_SE1506_Group7_StudentManagement/DataAccess/Repository/MarkRepository.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessObject
+namespace DataAccess.Repository
 {
-    class MajorObject
+    class MarkRepository
     {
     }
 }
